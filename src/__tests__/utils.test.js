538c7ce780f10b389c7107c138f81298984fec97
roselyne mwaniki <roselynewairimu30@gmail.com>
2024-11-16 19:05:32 +0300

    first commit

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,9 +1,29 @@
+// import { currentAgeForBirthYear } from "../utils";
+
+// describe("currentAgeForBirthYear", () => {
+//   it("returns the age of a person based on the year of birth", () => {
+//     const birthYear = 1984;
+//     const ageOfPerson = currentAgeForBirthYear(birthYear);
+//     expect(ageOfPerson).toBe(38);
+//   });
+// });
+
+
+// src/__tests__/utils.test.js
 import { currentAgeForBirthYear } from "../utils";
 
 describe("currentAgeForBirthYear", () => {
-  it("returns the age of a person based on the year of birth", () => {
+  it("returns the age of a person based on their year of birth", () => {
+    // Arrange
     const birthYear = 1984;
+    const currentYear = new Date().getFullYear();
+    const expectedAge = currentYear - birthYear;
+
+    // Act
     const ageOfPerson = currentAgeForBirthYear(birthYear);
-    expect(ageOfPerson).toBe(38);
+
+    // Assert
+    expect(ageOfPerson).toBe(expectedAge);
   });
 });
+
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,7 @@
 // Your code here
+// src/utils.js
+export function currentAgeForBirthYear(birthYear) {
+    const currentYear = new Date().getFullYear();
+    return currentYear - birthYear;
+  }
+  
